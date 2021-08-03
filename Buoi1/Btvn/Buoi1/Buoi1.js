d6e1e45d5bb3b26a1af0d47689fe81ab3c851690
= <=>
2021-08-03 13:35:41 +0700

    Bài tập buổi 1

--- Buoi1/Btvn/Buoi1/Buoi1.js
+++ Buoi1/Btvn/Buoi1/Buoi1.js
@@ -1,35 +1,35 @@
 // bài 1: Write a script to ask user about their name, and then say hi to them, say something nice to them if you want :)
-// let String = "Tên của bạn là gì vậy?";
-// alert(String);
+// let string = "Tên của bạn là gì vậy?";
+// alert(string);
 // let name = prompt("Nhập tên: ");
 // alert("Chào " + name);
 // alert(name + " bạn rất đẹp trai");
 // ------------------------
 // bài 2:Write a script that ask 2 things from users, their first name and last name, then greet them with their full name
-// let firstName=prompt("FirstName: ");
-// let Name=prompt("Name: ")
-// let fullName=firstName+Name;
-// alert("Xin chào "+fullName);
-//----------------------
-//bài 3:
-//Write a script that calculates the area of a square
-// let a=Number(prompt("Nhập cạnh a: "));
-// let S=a*a;
-// alert("The area of the square is: "+S);
-//bài 4:
-//Write a script that calculates the area of a circle
-// let r=Number(prompt("Bán kính: "));
-// const pi=3.14;
-// let shape=pi*r*r;
-// alert("circle area: "+ shape);
-//bài 5:
-//Write a script that converts Celsius (0C) into Fahrenheit (0F)
-let C=Number(prompt("Độ C: "));
-let F=Number(prompt("ĐỘ F: "));
-let cConvertF=(1.8*C)+32;
-alert("Độ C sang Độ F: "+cConvertF);
-let fConvertC=(F-32)/1.8;
-alert("ĐỘ F sang Độ C: "+fConvertC);
+// let firstName = prompt("FirstName: ");
+// let name = prompt("Name: ")
+// let fullName = firstName + name;
+// alert("Xin chào " + fullName);
+// ----------------------
+// bài 3:
+// Write a script that calculates the area of a square
+// let a = Number(prompt("Nhập cạnh a: "));
+// let s = a * a;
+// alert("The area of the square is: "+s);
+// bài 4:
+// Write a script that calculates the area of a circle
+// let r = Number(prompt("Bán kính: "));
+// const pi = 3.14;
+// let shape = pi * r * r;
+// alert("circle area: " + shape);
+// bài 5:
+// Write a script that converts Celsius (0C) into Fahrenheit (0F)
+let c = Number(prompt("Độ C: "));
+let f = Number(prompt("ĐỘ F: "));
+let cConvertF = (1.8 * c) + 32;
+alert("Độ C sang Độ F: " + cConvertF);
+let fConvertC = (f - 32) / 1.8;
+alert("ĐỘ F sang Độ C: " + fConvertC);
 
 
 
