3be78a8ead9d24451e129a64c5d5c94a2a84126c
= <=>
2021-08-06 00:22:38 +0700

    Bài tập buổi 2

--- Buoi1/Btvn/Buoi1/Buoi1.js
+++ Buoi1/Btvn/Buoi1/Buoi1.js
@@ -11,25 +11,25 @@
 // let fullName = firstName + name;
 // alert("Xin chào " + fullName);
 // ----------------------
-// bài 3:
-// Write a script that calculates the area of a square
+//bài 3:
+//Write a script that calculates the area of a square
 // let a = Number(prompt("Nhập cạnh a: "));
 // let s = a * a;
 // alert("The area of the square is: "+s);
-// bài 4:
-// Write a script that calculates the area of a circle
+// //bài 4:
+// //Write a script that calculates the area of a circle
 // let r = Number(prompt("Bán kính: "));
 // const pi = 3.14;
 // let shape = pi * r * r;
 // alert("circle area: " + shape);
-// bài 5:
-// Write a script that converts Celsius (0C) into Fahrenheit (0F)
-let c = Number(prompt("Độ C: "));
-let f = Number(prompt("ĐỘ F: "));
-let cConvertF = (1.8 * c) + 32;
-alert("Độ C sang Độ F: " + cConvertF);
-let fConvertC = (f - 32) / 1.8;
-alert("ĐỘ F sang Độ C: " + fConvertC);
+//bài 5:
+//Write a script that converts Celsius (0C) into Fahrenheit (0F)
+// let c = Number(prompt("Độ C: "));
+// let f = Number(prompt("ĐỘ F: "));
+// let cConvertF = (1.8 * c) + 32;
+// alert("Độ C sang Độ F: " + cConvertF);
+// let fConvertC = (f - 32) / 1.8;
+// alert("ĐỘ F sang Độ C: " + fConvertC);
 
 
 
